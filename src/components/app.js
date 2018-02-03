1de6d16bdb16c38336f0e10986dcde95773e7f16
mdalpozzo <marlin.dalpozzo@gmail.com>
2018-02-02 20:28:27 -0800

    got the side bar links to work

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,15 @@
 angular.module('video-player')
 
-.component('app', {
-  // TODO
-});
+  .component('app', {
+    // TODO
+    controller: function($window) {
+      this.videoList = window.exampleVideoData;
+      this.currentVideo = window.exampleVideoData[0];
+      
+      this.handleClick = (arg) => {
+        this.currentVideo = arg;
+      };
+    },
+    
+    templateUrl: 'src/templates/app.html'
+  });
